7c8e337a58d86da9e5812af6dc89974ae838ca94
shanu3742 <krshanu.95@gmail.com>
2021-05-29 14:42:33 -0700

    now we get content related to what we search ,open your console and verified the output

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -7,18 +7,31 @@ const Home = () => {
   const onInputchange = searchtext => {
     setInput(searchtext.target.value);
   };
-  // on enter key  pressed we get input value store
-  const onKeyDown = event => {
-    if (event.keyCode === 13) {
-      console.log(input);
-    }
-  };
 
   // onClick of search button we get input value store
 
-  const onSearch = () => {
-    console.log(input);
+  const onSearch = async () => {
+    // console.log(input);
+    // now we put input in search
+    const API = `http://api.tvmaze.com/search/shows?q=${input}`;
+    try {
+      const data = await fetch(API);
+      const realdata = await data.json();
+      console.log(realdata);
+    } catch (err) {
+      console.log(err);
+    }
   };
+
+  // on enter key  pressed we get input value store
+  const onKeyDown = event => {
+    if (event.keyCode === 13) {
+      // console.log(input);
+      // this function doing the same what onSearch function did so simply call onsearch function to performing same action here aslso
+      onSearch();
+    }
+  };
+
   return (
     <MainpageLayout>
       <input
