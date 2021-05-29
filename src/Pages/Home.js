b21be560602963d2cbd3024b82f63c807b551ee0
shanu3742 <krshanu.95@gmail.com>
2021-05-29 13:42:21 -0700

    when we press on search   button  then only we get value

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,12 +3,20 @@ import MainpageLayout from '../component/MainpageLayout';
 
 const Home = () => {
   const [input, setInput] = useState('');
+
   const onInputchange = searchtext => {
     setInput(searchtext.target.value);
+  };
+
+  const onSearch = () => {
+    console.log(input);
   };
   return (
     <MainpageLayout>
       <input type="text" onChange={onInputchange} value={input} />
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
     </MainpageLayout>
   );
 };
