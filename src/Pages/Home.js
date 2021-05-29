e11be8b0c625bc8b7961644e45077a9942d83167
shanu3742 <krshanu.95@gmail.com>
2021-05-29 13:55:13 -0700

    now we also get input value when we pressed enter key

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -7,13 +7,26 @@ const Home = () => {
   const onInputchange = searchtext => {
     setInput(searchtext.target.value);
   };
+  // on enter key  pressed we get input value store
+  const onKeyDown = event => {
+    if (event.keyCode === 13) {
+      console.log(input);
+    }
+  };
+
+  // onClick of search button we get input value store
 
   const onSearch = () => {
     console.log(input);
   };
   return (
     <MainpageLayout>
-      <input type="text" onChange={onInputchange} value={input} />
+      <input
+        type="text"
+        onChange={onInputchange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
       <button type="button" onClick={onSearch}>
         Search
       </button>
